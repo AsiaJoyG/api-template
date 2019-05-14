821c35bcddea8d03c929b9a3ab06550802e465ca
Asia Gagnon <asiajoyg@gmail.com>
2019-05-14 10:54:52 -0400

    more roots?

---
+++ app/components/AllCandies.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import { connect } from 'react-redux';
+import {getAllCandiesThunk} from "../reducers"
+import {Link} from 'react-router-dom'
+
+
+
+export default connect(, )(AllCandies);
+
+export default connect(, )(AllCandies);
+
+export default AllCandies;
+
+class AllCandies extends React.Component {
+  componentDidMount(){
+
+  }
+
+  render(){
+    let candies = this.props.candies
+    if(this.props.loading){return (<div>LOADING</div>)}
+    return(
+
+     <div>
+       {candies.map(candy =>
+       <div key = {candy.id}>
+        <hi> {candy.name}</hi>
+        <Link to= {`/candies/${candy.id}`}><img src = {candy.image}/></Link>
+        </div>
+        )}
+     </div>
+    )
+  }
+}
+
+
+ export default AllCandies;
+
+const mapState = state => ({
+  candies: state.candies,
+  loading: state.loading
+})
+
+
+const mapDispatch = (dispatch) => ({
+  getCandies: ()=> dispatch(getAllCandiesThunk())
+})
+
+export default connect(mapState,mapDispatch)(AllCandies);
---
+++ app/components/SingleCandy.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import {getSingleCandyThunk} from "../reducers"
+import {connect} from 'react-redux'
+
+class SingleCandy extends Component {
+
+componentDidMount() {
+this.props.getCandy(this.props.match.params.id)
+}
+  render() {
+    return (
+    <div>
+    <h1> {candy.name} </h1>
+    <img src= {candy.image}/>
+    </div>
+    );
+  }
+}
+
+const mapState = (state) => ({
+  candy: state.singleCandy
+});
+
+const mapDispatch = dispatch => ({
+getCandy: (id) => dispatch(getSingleCandyThunk(id))
+})
+
+
+export default connect(mapState,mapDispatch)(SingleCandy);
